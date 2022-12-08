1bf270adcdd5c4a2023a68ae89b4adc8fa0786a0
alphaseinor <alphaseinor@gmail.com>
2022-12-07 23:40:17 -0600

    modified code for day 1 part 2

--- 2022/day1/index.js
+++ 2022/day1/index.js
@@ -6,19 +6,22 @@ const readFile = promisify(fs.readFile)
 async function main() {
     data = await readFile('input.txt', "utf8")
     const parsed = data.split("\r\n").map(calorie => calorie !== "" ? parseInt(calorie) : "")
-    let highestCalories = 0
+    
     let tempCalories = 0
-    parsed.forEach(calorie => {
+    let calorieList = parsed.map(calorie => {
         if(calorie != ""){
             tempCalories += calorie
-            if(tempCalories > highestCalories){
-                highestCalories = tempCalories
-            }
         }else{
+            highestCalories = tempCalories
+            console.log(tempCalories, highestCalories)
             tempCalories = 0
+            return highestCalories
         }
-    })
-    console.log(highestCalories)
+    }).filter(calorie => calorie !== undefined)
+    calorieList.sort((a, b) => {
+        return a - b
+    }).reverse()
+    console.log(calorieList[0] + calorieList[1] + calorieList[2])
 }
 
 try {
